11c276e57c069a2c8cb6d38fac809303a16bff7f
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 21:05:51 +0300

    Add references to buttons and calculator display in the script

--- scripts/script.js
+++ scripts/script.js
@@ -36,9 +36,16 @@ let num1 = 0;
 let num2 = 0;
 let operation = "";
 
-num1 = prompt("num1 = ");
-num2 = prompt("num2 = ");
-console.log(`${num1} + ${num2} = ${operate(num1, num2, "+")}`);
-console.log(`${num1} - ${num2} = ${operate(num1, num2, "-")}`);
-console.log(`${num1} * ${num2} = ${operate(num1, num2, "*")}`);
-console.log(`${num1} / ${num2} = ${operate(num1, num2, "/")}`);
+const btn0 = document.querySelector("#btn0");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+
+const display = document.querySelector("#calc-display");
+display.textContent = "0";
