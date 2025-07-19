9d25dd262c4e338eef44983cd565dec065babfda
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 19:03:15 +0300

    Implement opreate function to perform operations on two numbers

--- scripts/script.js
+++ scripts/script.js
@@ -17,13 +17,28 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            break;
+    }
+}
+
 let num1 = 0;
 let num2 = 0;
 let operation = "";
 
-let x = prompt("x = ");
-let y = prompt("y = ");
-console.log(`${x} + ${y} = ${add(x, y)}`);
-console.log(`${x} - ${y} = ${subtract(x, y)}`);
-console.log(`${x} * ${y} = ${multiply(x, y)}`);
-console.log(`${x} / ${y} = ${divide(x, y)}`);
+num1 = prompt("num1 = ");
+num2 = prompt("num2 = ");
+console.log(`${num1} + ${num2} = ${operate(num1, num2, "+")}`);
+console.log(`${num1} - ${num2} = ${operate(num1, num2, "-")}`);
+console.log(`${num1} * ${num2} = ${operate(num1, num2, "*")}`);
+console.log(`${num1} / ${num2} = ${operate(num1, num2, "/")}`);
