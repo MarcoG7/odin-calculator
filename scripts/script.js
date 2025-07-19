dd45b7bf8a1952f57b187e5e3dedb56b2ec82fdd
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 18:50:15 +0300

    Create variables that will contain the operand and the two numbers

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,10 @@ function divide(a, b) {
     return a / b;
 }
 
+let num1 = 0;
+let num2 = 0;
+let operation = "";
+
 let x = prompt("x = ");
 let y = prompt("y = ");
 console.log(`${x} + ${y} = ${add(x, y)}`);
