75cbb16515fa93dbffb3e5f510e326ee083b48cd
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 18:43:02 +0300

    Implement function to perform subtraction on two numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 function add(a, b) {
-    if (typeof a !== Number) { a = parseFloat(a); }
-    if (typeof b !== Number) { b = parseFloat(b); }
     return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
 }
 
 let x = prompt("x = ");
 let y = prompt("y = ");
 console.log(`${x} + ${y} = ${add(x, y)}`);
+console.log(`${x} - ${y} = ${subtract(x, y)}`);
